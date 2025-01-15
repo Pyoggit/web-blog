847cd0dcbeccf03a0ca962dc85f8c72bf3ceb726
jangpyo <wkdvy122851@gmail.com>
2025-01-15 23:45:21 +0900

    [UPDATE] 인증실패 메시지 수정

--- board-back/src/main/java/com/pyo/board_back/config/WebsecurityConfig.java
+++ board-back/src/main/java/com/pyo/board_back/config/WebsecurityConfig.java
@@ -81,8 +81,8 @@ class FailedAuthenticationEntryPoint implements AuthenticationEntryPoint {
                         AuthenticationException authException) throws IOException, ServletException {
 
                 response.setContentType("application/json");
-                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-                response.getWriter().write("{\"code\": \"NP\",\"message\":\"Do not have permisson.\"}");
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.getWriter().write("{\"code\": \"AF\",\"message\":\"Authorization Failed\"}");
 
         }
 
