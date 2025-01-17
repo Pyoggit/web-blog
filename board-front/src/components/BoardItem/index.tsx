0dabf004ed92623617ca4548c2d7a7fb4d226db2
jangpyo <wkdvy122851@gmail.com>
2025-01-17 14:24:24 +0900

    [CREATE] 게시물리스트 컴포넌트 작성

---
+++ board-front/src/components/BoardItem/index.tsx
@@ -0,0 +1,65 @@
+import { BoardListItem } from '@/types/interface'
+import './style.css'
+//import { useNavigate } from 'react-router-dom';
+//import DefaultProfileImage from 'assets/image/default-profile-image.png';
+import DefaultProfileImage from "../../assets/image/default-profile-image.png";
+
+
+interface Props {
+    boardListItem: BoardListItem;
+  
+}
+
+
+// Board List Item 컴포넌트
+export default function BoardListItem({ boardListItem }:Props) {
+
+    const{  title, content, boardTitleImage} = boardListItem;
+    const { favoriteCount, commentCount, viewCount}=boardListItem;
+    const { writeDatetime, writerNickname, writerProfileImage}=boardListItem;
+
+
+    //const navigator = useNavigate();
+
+    // const onClickHandler = ()=>{
+    //     navigator(boardNumber);
+    // }
+    // render: Board List Item 컴포넌트 렌더링
+  return (
+    <div className='board-list-item'>
+      <div className='board-list-item-main-box'>
+        <div className='board-list-item-top'>
+          <div className='board-list-item-profile-box'>
+            <div
+              className='board-list-item-profile-image'
+              style={{ backgroundImage: `url(${writerProfileImage ? writerProfileImage : DefaultProfileImage})` }}
+            ></div>
+          </div>
+          <div className='board-list-item-write-box'>
+            <div className='board-list-item-nickname'>{writerNickname}</div>
+            <div className='board-list-item-write-date'>{writeDatetime}</div>
+          </div>
+        </div>
+        <div className='board-list-item-middle'>
+          <div className='board-list-item-title'>{title}</div>
+          <div className='board-list-item-content'>
+            {content}
+          </div>
+        </div>
+        <div className='board-list-item-bottom'>
+          <div className='board-list-item-counts'>
+            {`댓글 ${commentCount}. 좋아요 ${favoriteCount}. 조회수 ${viewCount}`}
+          </div>
+        </div>
+      </div>
+      <div className='board-list-item-image-box'>
+        {boardTitleImage !== null &&(
+            <div
+              className='board-list-item-image'
+              style={{ backgroundImage: `url(${boardTitleImage})` }}
+            ></div>
+        )}
+      </div>
+    </div>
+  )
+}
