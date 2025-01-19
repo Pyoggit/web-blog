2d91b05723518d52d6598359df903938d13a6097
jangpyo <wkdvy122851@gmail.com>
2025-01-20 01:28:21 +0900

    [CREATE] 좋아요 리스트 mock data 작성

---
+++ board-front/src/mocks/favorite-list.mock.ts
@@ -0,0 +1,29 @@
+const favoriteListMock = [
+    {
+        "email": "example@example.com" ,
+        "nickname": "젠인 토우지",
+        "profileImage": null,
+    },
+    {
+        "email": "example@example.com" ,
+        "nickname": "젠인 토우지",
+        "profileImage": null,
+    },
+    {
+        "email": "example@example.com" ,
+        "nickname": "젠인 토우지",
+        "profileImage": null,
+    },
+    {
+        "email": "example@example.com" ,
+        "nickname": "젠인 토우지",
+        "profileImage": null,
+    },
+    {
+        "email": "example@example.com" ,
+        "nickname": "젠인 토우지",
+        "profileImage": null,
+    },
+];
+
+export default favoriteListMock
