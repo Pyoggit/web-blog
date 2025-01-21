5abb3a1a6208b9337acd91e0c4e6390b752384d6
jangpyo <wkdvy122851@gmail.com>
2025-01-21 22:46:55 +0900

    [CREATE] Footer 공통 컴포넌트 작성

---
+++ board-front/src/layouts/Footer/index.tsx
@@ -0,0 +1,46 @@
+import './style.css'
+
+//  component: 푸터 레이아웃//
+export default function Footer(){
+
+    // event handler: 인스타 아이콘 버튼 이벤트 처리//
+    const onInstaIconButtonClickHandler =() =>{
+        window.open('http://www.instagram.com');
+    };
+    
+    // event handler: 네이버 블로그 아이콘 버튼 이벤트 처리//
+    const onNaverBlogIconButtonClickHandler =() =>{
+        window.open('http://blog.naver.com');
+    };
+    
+
+    
+// render: 푸터 레이아웃 렌더링 //
+return(
+    <div id='footer'>
+        <div className='footer-container'>
+            <div className='footer-top'>
+                <div className='footer-logo-box'>
+                    <div className='icon-box'>
+                        <div className='icon logo-light-icon'></div>
+                    </div>
+                    <div className='footer-logo-text'>{'pyoard'}</div>
+                </div>
+                <div className='footer-link-box'>  
+                    <div className='footer-email-link'>{'wkdvy122851@gmail.com'}</div>
+                    <div className='icon-button' onClick={onInstaIconButtonClickHandler}>
+                        <div className='icon insta-icon'></div>
+                    </div>
+                    <div className='icon-button' onClick={onNaverBlogIconButtonClickHandler}>
+                        <div className='icon naver-blog-icon'></div>
+                    </div>
+                </div>
+            </div>
+            <div className='footer-bottom'>
+                <div className='footer-copyright'>{'Copyright © 2025 pyo. All Rights Reserved.'}</div>
+            </div>
+        </div>
+    </div>
+)
+
+}
