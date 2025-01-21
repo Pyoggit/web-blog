465b6acea1f0debfb137bcc784660ae93beebdda
jangpyo <wkdvy122851@gmail.com>
2025-01-22 01:13:08 +0900

    [UPDATE] 전체 레이아웃 컴포넌트 및 렌더링 처리

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,24 +1,47 @@
 //import BoardItem from './components/BoardItem';
 //import Top3Item from './components/Top3Item';
 //import CommentItem from './components/CommentItem';
-import FavoriteItem from './components/FavoriteItem';
+// import FavoriteItem from './components/FavoriteItem';
+import { Route, Routes } from 'react-router-dom';
 import './App.css'
-import { favoriteListMock, commentListMock, top3BoardListMock, latestBoardListMock } from './mocks';
-import InputBox from './components/InputBox';
-import { useState } from 'react';
+import Authentication from './views/Authentication';
+import Main from './views/Main';
+import Search from './views/Search';
+import User from './views/User';
+import BoardWrite from './views/Board/Write';
+import BoardUpdate from './views/Board/Update';
+import BoardDetail from './views/Board/Detail';
+import Container from './layouts/Container';
+import { AUTH_PATH, BOARD_DETAIL_PATH, BOARD_PATH, BOARD_UPDATE_PATH, BOARD_WRITE_PATH, MAIN_PATH, SEARCH_PATH, USER_PATH } from './constant';
+// import { favoriteListMock, commentListMock, top3BoardListMock, latestBoardListMock } from './mocks';
+// import InputBox from './components/InputBox';
+//import { useState } from 'react';
+//import Footer from './layouts/Footer';
 
 
 
-//{latestBoardListMock.map(BoardListItem => <BoardItem boardListItem = {BoardListItem} />  )}
+// component: Application 컴포넌트 //
 function App() {
-
-  const [value, setValue] = useState<string>('');
+  
+  // render: Application 컴포넌트 렌더링 //
 
   return (
-    <>
-    <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요' value={value} error={false} setValue={setValue} message='aaaa' />
-    </>
-  )
+    <Routes>
+      <Route element={<Container/>}>
+      <Route path={MAIN_PATH()} element={<Main />} />
+        <Route path={AUTH_PATH()} element={<Authentication />} />
+        <Route path={SEARCH_PATH(':{searchWord')} element={<Search />} />
+        <Route path={USER_PATH(':userEmail')} element={<User />} />
+        <Route path={BOARD_PATH()}>
+            <Route path={BOARD_WRITE_PATH()} element={<BoardWrite />} />
+            <Route path={BOARD_DETAIL_PATH(':boardNumber')} element={<BoardDetail />} />
+            <Route path={BOARD_UPDATE_PATH(':boardNumber')} element={<BoardUpdate />} />
+        </Route>
+        <Route path='*' element={<h1> 404 Not Found</h1>} />
+      </Route>  
+    </Routes>
+);
+
 }
 
 export default App;
