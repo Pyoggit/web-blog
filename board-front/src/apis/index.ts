c44e8963213b9c0f3d5d5745eaede630eb66dc01
jangpyo <wkdvy122851@gmail.com>
2025-01-24 10:47:39 +0900

    [CREATE] board-back과 로그인 API 연동

--- board-front/src/apis/index.ts
+++ board-front/src/apis/index.ts
@@ -1,1 +1,16 @@
-export const tmp = '';
+import { SignInReqeustDto, SignUpRequestDto } from "./request/auth";
+
+const DOMAIN = 'http://localhost:4001';
+
+const API_DOMAIN = `${DOMAIN}/api/v1`;
+
+const SIGN_IN_URL = () => `${API_DOMAIN}/auth/sign-in`;
+const SIGN_UP_URL = () => `${API_DOMAIN}/auth/sign-up`;
+
+export const signInRequest = (requestBody: SignInReqeustDto) => {
+    // Sign in request logic here
+};
+
+export const signUpRequest = (requestBody: SignUpRequestDto) => {
+    // Sign up request logic here
+};
