ed11ab231443be1bd8f7d5a712b5da7880888bfb
Jang Pyo <wkdvy122851@gmail.com>
2025-03-11 23:28:59 +0900

    [UPDATE] accessToken을 cookie에 저장

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,27 +1,53 @@
-//import BoardItem from './components/BoardItem';
-//import Top3Item from './components/Top3Item';
-//import CommentItem from './components/CommentItem';
-// import FavoriteItem from './components/FavoriteItem';
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import Authentication from './views/Authentication';
 import Main from './views/Main';
 import Search from './views/Search';
-import User from './views/User';
+import UserP from './views/User';
 import BoardWrite from './views/Board/Write';
 import BoardUpdate from './views/Board/Update';
 import BoardDetail from './views/Board/Detail';
 import Container from './layouts/Container';
 import { AUTH_PATH, BOARD_DETAIL_PATH, BOARD_PATH, BOARD_UPDATE_PATH, BOARD_WRITE_PATH, MAIN_PATH, SEARCH_PATH, USER_PATH } from './constant';
-// import { favoriteListMock, commentListMock, top3BoardListMock, latestBoardListMock } from './mocks';
-// import InputBox from './components/InputBox';
-//import { useState } from 'react';
-//import Footer from './layouts/Footer';
+import { useCookies } from 'react-cookie';
+import { useLoginUserStore } from './stores';
+import { getSignInUserRequest } from './apis';
+import { GetSignInUserResponseDto } from './apis/response/user';
+import { ResponseDto } from './apis/response';
+import { User } from './types/interface';
 
 
 
 // component: Application 컴포넌트 //
 function App() {
+
+  // state: 로그인 유저 전역 상태 //
+  const { setLoginUser, resetLoginUser } = useLoginUserStore();
+  // state: cookie 상태 //
+  const [cookies, setCookie] = useCookies;
+
+  // function: get sign in user response 처리 함수 //
+  const getSignInUserResponse = (responseBody: GetSignInUserResponseDto | ResponseDto | null) => {
+    if (!responseBody)return;
+    const { code } = responseBody;
+    if(code === 'AF'|| code === 'NU' || code === 'DBE'  ){
+      resetLoginUser();
+      return;
+    }
+    const loginUser: User = { ...responseBody as GetSignInUserResponseDto };
+    setLoginUser(loginUser);
+  }
+
+  // effect: accessToken cookie 값이 변경될 때 마다 실행할 함수 //
+  useEffect(() => {
+    if(!cookies.accessToken){
+      resetLoginUser();
+      return;
+    }
+    getSignInUserRequest(cookies.accessToken).then(getSignInUserResponse);
+  }, [cookies.accessToken]);
+
   
   // render: Application 컴포넌트 렌더링 //
 
@@ -31,7 +57,7 @@ function App() {
       <Route path={MAIN_PATH()} element={<Main />} />
         <Route path={AUTH_PATH()} element={<Authentication />} />
         <Route path={SEARCH_PATH(':{searchWord')} element={<Search />} />
-        <Route path={USER_PATH(':userEmail')} element={<User />} />
+        <Route path={USER_PATH(':userEmail')} element={<UserP />} />
         <Route path={BOARD_PATH()}>
             <Route path={BOARD_WRITE_PATH()} element={<BoardWrite />} />
             <Route path={BOARD_DETAIL_PATH(':boardNumber')} element={<BoardDetail />} />
