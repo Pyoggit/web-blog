4f2436fb3d66dab104e8c7eb69979c63782b6011
Jang Pyo <wkdvy122851@gmail.com>
2025-03-25 22:38:59 +0900

    [UPDATE] 게시물 작성 상태(Effect) 추가

--- board-front/src/views/Board/Write/index.tsx
+++ board-front/src/views/Board/Write/index.tsx
@@ -1,5 +1,6 @@
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './style.css'
+import { useBoardStore } from '@/stores';
 
 // component: 게시물 작성 화면 컴포넌트 //
 export default function BoardWrite(){
@@ -9,7 +10,19 @@ export default function BoardWrite(){
  const contentRef = useRef<HTMLTextAreaElement | null>(null);
  // state: 이미지 입력 요소 참조 상태 //
  const imageInputRef = useRef<HTMLInputElement | null>(null);
+ // state: 게시물 상태 //
+ const {title, setTitle} = useBoardStore();
+ const {content, setContent} = useBoardStore();
+ const {boardImageFileList, setBoardImageFileList} = useBoardStore();
+ const {resetBoard} = useBoardStore();
 
+ // state: 게시물 이미지 미리보기 URL 상태 //
+ const [imageUrls, setImageUrls] = useState<string[]>([]);
+
+ // effect: 마운트시 실행할 함수 //
+ useEffect(() => {
+    resetBoard();
+ },[]);
 
     // render: 게시물 작성 화면 컴포넌트 렌더링 //
     return(
@@ -17,11 +30,11 @@ export default function BoardWrite(){
             <div className='board-write-container'>
                 <div className='board-write-box'>
                     <div className='board-write-title-box'>
-                        <input className='board-write-title-input' type='text' placeholder='제목을 작성해주세요.' />
+                        <input className='board-write-title-input' type='text' placeholder='제목을 작성해주세요.' value={title}/>
                     </div>
                     <div className='divider'></div>
                     <div className='board-write-content-box'>
-                        <textarea ref={contentRef} className='board-write-content-textarea' placeholder='본문을 작성해주세요.' />
+                        <textarea ref={contentRef} className='board-write-content-textarea' placeholder='본문을 작성해주세요.' value={content} />
                         <div className='icon-button'>
                             <div className='image-box-light-icon'></div>
                         </div>
