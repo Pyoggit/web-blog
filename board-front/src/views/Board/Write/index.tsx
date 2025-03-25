f4effe8d2d1f8b9afde7867e3b05d2066ba47f2a
Jang Pyo <wkdvy122851@gmail.com>
2025-03-25 22:25:25 +0900

    [UPDATE] placeholder 추가

--- board-front/src/views/Board/Write/index.tsx
+++ board-front/src/views/Board/Write/index.tsx
@@ -8,15 +8,15 @@ export default function BoardWrite(){
             <div className='board-write-container'>
                 <div className='board-write-box'>
                     <div className='board-write-title-box'>
-                        <input className='board-write-title-input' />
+                        <input className='board-write-title-input' type='text' placeholder='제목을 작성해주세요.' />
                     </div>
                     <div className='divider'></div>
                     <div className='board-write-content-box'>
-                        <textarea className='board-write-content-textarea' />
+                        <textarea className='board-write-content-textarea' placeholder='본문을 작성해주세요.' />
                         <div className='icon-button'>
                             <div className='image-box-light-icon'></div>
                         </div>
-                        <input />
+                        <input type='file' accept='image/*' style={{display: 'none'}}/>
                     </div>
                     <div className='board-write-images-box'>
                         <div className='board-write-image-box'>
