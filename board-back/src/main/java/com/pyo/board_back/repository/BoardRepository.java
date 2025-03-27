f7e13aa465bda6c3bba8207fcf1ce98999313741
Jang Pyo <wkdvy122851@gmail.com>
2025-03-28 00:05:50 +0900

    [UPDATE] 게시글 존재 여부 확인 메서드 추가

--- board-back/src/main/java/com/pyo/board_back/repository/BoardRepository.java
+++ board-back/src/main/java/com/pyo/board_back/repository/BoardRepository.java
@@ -10,6 +10,8 @@ import com.pyo.board_back.repository.resultSet.GetBoardResultSet;
 @Repository
 public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
 
+    boolean existsByBoardNumber(Integer boardNumber);
+
     BoardEntity findByBoardNumber(Integer boardNumber);
 
     @Query(value = "SELECT " +
