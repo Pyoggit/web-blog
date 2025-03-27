49c8a0f1548f4448e7305cdc339ceec2b780f0e0
Jang Pyo <wkdvy122851@gmail.com>
2025-03-27 15:55:26 +0900

    [UPDATE] 게시글이 존재하지 않을 때의 에러 내용 추가

--- board-back/src/main/java/com/pyo/board_back/dto/response/board/GetBoardResponseDto.java
+++ board-back/src/main/java/com/pyo/board_back/dto/response/board/GetBoardResponseDto.java
@@ -2,6 +2,9 @@ package com.pyo.board_back.dto.response.board;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import com.pyo.board_back.common.ResponseCode;
 import com.pyo.board_back.common.ResponseMessage;
 import com.pyo.board_back.dto.response.ResponseDto;
@@ -22,6 +25,16 @@ public class GetBoardResponseDto extends ResponseDto {
 
     private GetBoardResponseDto() {
         super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+    }
+
+    public static ResponseEntity<GetBoardResponseDto> success() {
+        GetBoardResponseDto result = new GetBoardResponseDto();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    public static ResponseEntity<ResponseDto> noExistBoard() {
+        ResponseDto result = new ResponseDto(ResponseCode.NOT_EXISTED_BOARD, ResponseMessage.NOT_EXISTED_BOARD);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
     }
 
 }
