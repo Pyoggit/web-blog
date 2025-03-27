3fd5c05feee385b938cefe097e54649da8e53403
Jang Pyo <wkdvy122851@gmail.com>
2025-03-28 00:13:53 +0900

    [UPDATE] 댓글 등록, 좋아요 토글 및 목록 조회 서비스 로직 추가

--- board-back/src/main/java/com/pyo/board_back/service/implement/BoardServiceImplement.java
+++ board-back/src/main/java/com/pyo/board_back/service/implement/BoardServiceImplement.java
@@ -7,15 +7,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.pyo.board_back.dto.request.board.PostBoardRequestDto;
+import com.pyo.board_back.dto.request.board.PostCommentRequestDto;
 import com.pyo.board_back.dto.response.ResponseDto;
 import com.pyo.board_back.dto.response.board.GetBoardResponseDto;
+import com.pyo.board_back.dto.response.board.GetCommentListResponseDto;
+import com.pyo.board_back.dto.response.board.GetFavoriteListResponseDto;
 import com.pyo.board_back.dto.response.board.PostBoardResponseDto;
+import com.pyo.board_back.dto.response.board.PostCommentResponseDto;
+import com.pyo.board_back.dto.response.board.PutFavoriteResponseDto;
 import com.pyo.board_back.entity.BoardEntity;
+import com.pyo.board_back.entity.CommentEntity;
+import com.pyo.board_back.entity.FavoriteEntity;
 import com.pyo.board_back.entity.ImageEntity;
 import com.pyo.board_back.repository.BoardRepository;
+import com.pyo.board_back.repository.CommentRepository;
+import com.pyo.board_back.repository.FavoriteRepository;
 import com.pyo.board_back.repository.ImageRepository;
 import com.pyo.board_back.repository.UserRepository;
 import com.pyo.board_back.repository.resultSet.GetBoardResultSet;
+import com.pyo.board_back.repository.resultSet.GetCommentListResultSet;
+import com.pyo.board_back.repository.resultSet.GetFavoriteListResultSet;
 import com.pyo.board_back.service.BoardService;
 
 import lombok.RequiredArgsConstructor;
@@ -27,6 +38,8 @@ public class BoardServiceImplement implements BoardService {
     private final BoardRepository boardRepository;
     private final UserRepository userRepository;
     private final ImageRepository imageRepository;
+    private final CommentRepository commentRepository;
+    private final FavoriteRepository favoriteRepository;
 
     @Override
     public ResponseEntity<? super GetBoardResponseDto> getBoard(Integer boardNumber) {
@@ -50,6 +63,45 @@ public class BoardServiceImplement implements BoardService {
             return ResponseDto.databaseError();
         }
         return GetBoardResponseDto.success(resultSet, imageEntities);
+    }
+
+    @Override
+    public ResponseEntity<? super GetFavoriteListResponseDto> getFavoriteList(Integer boardNumber) {
+
+        List<GetFavoriteListResultSet> resultSets = new ArrayList<>();
+
+        try {
+            boolean existedBoard = boardRepository.existsByBoardNumber(boardNumber);
+            if (!existedBoard)
+                return GetFavoriteListResponseDto.noExistBoard();
+
+            resultSets = favoriteRepository.getFavoriteList(boardNumber);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+        return GetFavoriteListResponseDto.success(resultSets);
+    }
+
+    @Override
+    public ResponseEntity<? super GetCommentListResponseDto> getCommentList(Integer boardNumber) {
+        List<GetCommentListResultSet> resultSets = new ArrayList<>();
+
+        try {
+            boolean existedBoard = boardRepository.existsByBoardNumber(boardNumber);
+            if (!existedBoard)
+                return GetCommentListResponseDto.noExistBoard();
+
+            resultSets = commentRepository.getCommentList(boardNumber);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return GetCommentListResponseDto.success(resultSets);
+
     }
 
     @Override
@@ -80,5 +132,64 @@ public class BoardServiceImplement implements BoardService {
         }
 
         return PostBoardResponseDto.success();
+    }
+
+    @Override
+    public ResponseEntity<? super PostCommentResponseDto> postComment(PostCommentRequestDto dto, Integer boardNumber,
+            String email) {
+
+        try {
+            BoardEntity boardEntity = boardRepository.findByBoardNumber(boardNumber);
+            if (boardEntity == null)
+                return PostCommentResponseDto.noExistBoard();
+
+            boolean existedUser = userRepository.existsByEmail(email);
+            if (!existedUser)
+                return PostCommentResponseDto.noExistUser();
+
+            CommentEntity commentEntity = new CommentEntity(dto, boardNumber, email);
+            commentRepository.save(commentEntity);
+
+            boardEntity.increaseCommentCount();
+            boardRepository.save(boardEntity);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return PostCommentResponseDto.success();
+
+    }
+
+    @Override
+    public ResponseEntity<? super PutFavoriteResponseDto> putFavorite(Integer boardNumber, String email) {
+        try {
+
+            boolean existedUser = userRepository.existsByEmail(email);
+            if (!existedUser)
+                return PutFavoriteResponseDto.noExistUser();
+
+            BoardEntity boardEntity = boardRepository.findByBoardNumber(boardNumber);
+            if (boardEntity == null)
+                return PutFavoriteResponseDto.noExistBoard();
+
+            FavoriteEntity favoriteEntity = favoriteRepository.findByBoardNumberAndUserEmail(boardNumber, email);
+            if (favoriteEntity == null) {
+                favoriteEntity = new FavoriteEntity(email, boardNumber);
+                favoriteRepository.save(favoriteEntity);
+                boardEntity.increaseFavoriteCount();
+            } else {
+                favoriteRepository.delete(favoriteEntity);
+                boardEntity.decreaseFavoriteCount();
+            }
+
+            boardRepository.save(boardEntity);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+        return PutFavoriteResponseDto.success();
     }
 }
