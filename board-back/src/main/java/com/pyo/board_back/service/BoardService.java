322a973924718c99d6f4a90fb83743d20cc248f0
Jang Pyo <wkdvy122851@gmail.com>
2025-03-28 00:01:29 +0900

    [UPDATE] 댓글 작성 및 좋아요 등록 서비스 인터페이스 메서드 정의

--- board-back/src/main/java/com/pyo/board_back/service/BoardService.java
+++ board-back/src/main/java/com/pyo/board_back/service/BoardService.java
@@ -3,11 +3,26 @@ package com.pyo.board_back.service;
 import org.springframework.http.ResponseEntity;
 
 import com.pyo.board_back.dto.request.board.PostBoardRequestDto;
+import com.pyo.board_back.dto.request.board.PostCommentRequestDto;
 import com.pyo.board_back.dto.response.board.GetBoardResponseDto;
+import com.pyo.board_back.dto.response.board.GetCommentListResponseDto;
+import com.pyo.board_back.dto.response.board.GetFavoriteListResponseDto;
 import com.pyo.board_back.dto.response.board.PostBoardResponseDto;
+import com.pyo.board_back.dto.response.board.PostCommentResponseDto;
+import com.pyo.board_back.dto.response.board.PutFavoriteResponseDto;
 
 public interface BoardService {
     ResponseEntity<? super GetBoardResponseDto> getBoard(Integer boardNumber);
 
+    ResponseEntity<? super GetFavoriteListResponseDto> getFavoriteList(Integer boardNumber);
+
+    ResponseEntity<? super GetCommentListResponseDto> getCommentList(Integer boardNumber);
+
     ResponseEntity<PostBoardResponseDto> postBoard(PostBoardRequestDto dto, String email);
+
+    ResponseEntity<? super PostCommentResponseDto> postComment(PostCommentRequestDto dto, Integer boardNumber,
+            String email);
+
+    ResponseEntity<? super PutFavoriteResponseDto> putFavorite(Integer boardNumber, String email);
+
 }
