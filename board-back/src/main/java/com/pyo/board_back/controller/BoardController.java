3fd8feaade158ab27253b0a486204c5ee8990463
Jang Pyo <wkdvy122851@gmail.com>
2025-03-27 17:00:52 +0900

    [UPDATE] 게시글 조회 API 엔드포인트 추가

--- board-back/src/main/java/com/pyo/board_back/controller/BoardController.java
+++ board-back/src/main/java/com/pyo/board_back/controller/BoardController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pyo.board_back.dto.request.board.PostBoardRequestDto;
+import com.pyo.board_back.dto.response.board.GetBoardResponseDto;
 import com.pyo.board_back.dto.response.board.PostBoardResponseDto;
 import com.pyo.board_back.service.BoardService;
 
@@ -14,7 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/v1/board")
@@ -23,13 +25,19 @@ public class BoardController {
 
     private final BoardService boardService;
 
+    @GetMapping("/{boardNumber}")
+    public ResponseEntity<? super GetBoardResponseDto> getBoard(
+            @PathVariable("boardNumber") Integer boardNumber) {
+        ResponseEntity<? super GetBoardResponseDto> response = boardService.getBoard(boardNumber);
+        return response;
+    }
+
     @PostMapping("")
     public ResponseEntity<? super PostBoardResponseDto> postBoard(
-        @RequestBody @Valid PostBoardRequestDto requestBody,
-        @AuthenticationPrincipal String email
-    ) {
+            @RequestBody @Valid PostBoardRequestDto requestBody,
+            @AuthenticationPrincipal String email) {
         ResponseEntity<? super PostBoardResponseDto> response = boardService.postBoard(requestBody, email);
         return response;
     }
-    
+
 }
