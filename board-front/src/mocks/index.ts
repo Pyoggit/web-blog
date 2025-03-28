e222e3d1a8c011afd30a86283dce2fbf72b2cdbc
Jang Pyo <wkdvy122851@gmail.com>
2025-03-29 02:19:15 +0900

    [UPDATE] 게시글 목업 데이터 추가

--- board-front/src/mocks/index.ts
+++ board-front/src/mocks/index.ts
@@ -2,8 +2,10 @@ import latestBoardListMock from './latest-board-list.mock';
 import top3BoardListMock from './top3-board-list-mock';
 import commentListMock from './comment-list.mock';
 import favoriteListMock from './favorite-list.mock';
+import boardMock from './board.mock';
 
 export{
+    boardMock,
     latestBoardListMock,
     top3BoardListMock,
     commentListMock,
