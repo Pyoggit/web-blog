4c6445a4823055e88c00782ea474dacf22db0de3
Jang Pyo <wkdvy122851@gmail.com>
2025-03-29 03:03:28 +0900

    [UPDATE] 댓글 등록 응답 인터페이스 PostCommentResponseDto 추가

--- board-front/src/apis/response/board/index.ts
+++ board-front/src/apis/response/board/index.ts
@@ -2,6 +2,8 @@ import GetBoardResponseDto from "./get-board.response.dto";
 import GetFavoriteListResponseDto from "./get-favorite-list.response.dto";
 import GetCommentListResponseDto from "./get-comment-list.response.dto";
 import PostBoardResponseDto from "./post-board.response.dto";
+import PostCommentResponseDto from "./post-comment.response.dto";
+import PutFavoriteResponseDto from "./put-favorite.response.dto";
 import IncreaseViewCountResponseDto from "./increase-view-count.response.dto";
 
 export type {
@@ -9,5 +11,7 @@ export type {
     GetFavoriteListResponseDto,
     GetCommentListResponseDto,
     PostBoardResponseDto,
+    PostCommentResponseDto,
+    PutFavoriteResponseDto,
     IncreaseViewCountResponseDto
 }
