7065c6316641ea3ae55db2824ff52de0e10fa853
Jang Pyo <wkdvy122851@gmail.com>
2025-03-29 02:19:53 +0900

    [UPDATE] 게시글 상세, 조회수 증가, 좋아요 목록, 댓글 목록 요청 함수 추가

--- board-front/src/apis/index.ts
+++ board-front/src/apis/index.ts
@@ -3,8 +3,8 @@ import { SignInRequestDto, SignUpRequestDto } from "./request/auth";
 import { SignInResponseDto , SignUpResponseDto} from "./response/auth";
 import { ResponseDto } from "./response";
 import { GetSignInUserResponseDto } from "./response/user";
-import { PostBoardResponseDto } from "./response/board";
-import { PostBoardRequestDto } from "./request/board";
+import { PostBoardResponseDto, GetBoardResponseDto, IncreaseViewCountResponseDto, GetFavoriteListResponseDto, GetCommentListResponseDto } from "./response/board";
+import { PostBoardRequestDto} from "./request/board";
 
 const DOMAIN = 'http://localhost:4001';
 
@@ -64,7 +64,69 @@ export const getSignInUserRequest = async (accessToken: string) => {
     return result;
 }
 
+const GET_BOARD_URL = (boardNumber: number | string) => `${API_DOMAIN}/board/${boardNumber}`;
+const INCREASE_VIEW_COUNT_URL = (boardNumber: number | string) => `${API_DOMAIN}/board/${boardNumber}/increase-view-count`;
+const GET_FAVORITE_LIST_URL = (boardNumber: number | string) => `${API_DOMAIN}/board/${boardNumber}/favorite-list`;
+const GET_COMMENT_LIST_URL = (boardNumber: number | string) => `${API_DOMAIN}/board/${boardNumber}/comment-list`;
 const POST_BOARD_URL = () => `${API_DOMAIN}/board`;
+
+export const getBoardRequest = async (boardNumber: number | string) => {
+    const result = await axios.get(GET_BOARD_URL(boardNumber))
+    .then(response => {
+        const responseBody: GetBoardResponseDto = response.data;
+        return responseBody;
+    })
+    .catch(error => {
+        if(!error.response) return null;
+        const responseBody: ResponseDto = error.response.data;
+        return responseBody;
+    })
+    return result;
+}
+
+export const increaseViewCountRequest = async (boardNumber: number | string) => {
+    const result = await axios.get(INCREASE_VIEW_COUNT_URL(boardNumber))
+        .then(response => {
+            const responseBody: IncreaseViewCountResponseDto = response.data;
+            return responseBody;
+        })
+        .catch(error => {
+            if(!error.response) return null;
+            const responseBody: ResponseDto = error.response.data;
+            return responseBody;
+        })
+        return result;
+}
+
+export const getFavoriteListRequest = async (boardNumber: number | string) => {
+    const result = await axios.get(GET_FAVORITE_LIST_URL(boardNumber))
+        .then(response => {
+            const responseBody:GetFavoriteListResponseDto = response.data;
+            return responseBody;
+        })
+        .catch(error =>{
+            if(!error.response) return null;
+            const responseBody: ResponseDto = error.response.data;
+            return responseBody;
+
+        });
+    return result;
+}
+
+export const getCommentListRequest = async (boardNumber: number | string) => {
+    const result = await axios.get(GET_COMMENT_LIST_URL(boardNumber))
+        .then(response => {
+            const responseBody:GetCommentListResponseDto = response.data;
+            return responseBody;
+        })
+        .catch(error =>{
+            if(!error.response) return null;
+            const responseBody: ResponseDto = error.response.data;
+            return responseBody;
+
+        });
+    return result;
+}
 
 export const PostBoardRequest = async (requestBody: PostBoardRequestDto, accessToken: string ) =>  {
     const result = await axios.post(POST_BOARD_URL(), requestBody, authorization(accessToken))
--- board-front/src/apis/response/board/index.ts
+++ board-front/src/apis/response/board/index.ts
@@ -1,5 +1,13 @@
+import GetBoardResponseDto from "./get-board.response.dto";
+import GetFavoriteListResponseDto from "./get-favorite-list.response.dto";
+import GetCommentListResponseDto from "./get-comment-list.response.dto";
 import PostBoardResponseDto from "./post-board.response.dto";
+import IncreaseViewCountResponseDto from "./increase-view-count.response.dto";
 
 export type {
-    PostBoardResponseDto
+    GetBoardResponseDto,
+    GetFavoriteListResponseDto,
+    GetCommentListResponseDto,
+    PostBoardResponseDto,
+    IncreaseViewCountResponseDto
 }
