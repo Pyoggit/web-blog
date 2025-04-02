a4635db025783c0ce2f2ad178b083887d3b020f7
Jang Pyo <wkdvy122851@gmail.com>
2025-04-02 16:54:44 +0900

    [UPDATE] 연관 검색어 조회 서비스 메서드 추가

--- board-back/src/main/java/com/pyo/board_back/service/SearchService.java
+++ board-back/src/main/java/com/pyo/board_back/service/SearchService.java
@@ -3,8 +3,11 @@ package com.pyo.board_back.service;
 import org.springframework.http.ResponseEntity;
 
 import com.pyo.board_back.dto.response.search.GetPopularListResponseDto;
+import com.pyo.board_back.dto.response.search.GetRelationListResponseDto;
 
 public interface SearchService {
 
     ResponseEntity<? super GetPopularListResponseDto> getPopularList();
+
+    ResponseEntity<? super GetRelationListResponseDto> getRelationList(String searchWord);
 }
