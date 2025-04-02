b57406de5bd6863111a25ccf6d007b9cd0744769
Jang Pyo <wkdvy122851@gmail.com>
2025-04-02 16:55:36 +0900

    [UPDATE] 닉네임 및 프로필 이미지 수정 서비스 메서드 추가

--- board-back/src/main/java/com/pyo/board_back/service/UserService.java
+++ board-back/src/main/java/com/pyo/board_back/service/UserService.java
@@ -1,11 +1,23 @@
 package com.pyo.board_back.service;
 
+import com.pyo.board_back.dto.request.user.PatchNicknameRequestDto;
+import com.pyo.board_back.dto.request.user.PatchProfileImageRequestDto;
 import com.pyo.board_back.dto.response.user.GetSignInUserResponseDto;
+import com.pyo.board_back.dto.response.user.GetUserResponseDto;
+import com.pyo.board_back.dto.response.user.PatchNicknameResponseDto;
+import com.pyo.board_back.dto.response.user.PatchProfileImageResponseDto;
 
 import org.springframework.http.ResponseEntity;
 
 public interface UserService {
-    
+
+    ResponseEntity<? super GetUserResponseDto> getUser(String email);
+
     ResponseEntity<? super GetSignInUserResponseDto> getSignInUser(String email);
-    
+
+    ResponseEntity<? super PatchNicknameResponseDto> patchNickname(PatchNicknameRequestDto dto, String email);
+
+    ResponseEntity<? super PatchProfileImageResponseDto> patchProfileImage(PatchProfileImageRequestDto dto,
+            String email);
+
 }
