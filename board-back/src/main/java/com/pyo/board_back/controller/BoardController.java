252d2fa7595a52fb270c17640129e16115d0234d
Jang Pyo <wkdvy122851@gmail.com>
2025-04-03 19:41:28 +0900

    [UPDATE] 게시글 검색 및 유저별 게시글 조회 API 엔드포인트 추가

--- board-back/src/main/java/com/pyo/board_back/controller/BoardController.java
+++ board-back/src/main/java/com/pyo/board_back/controller/BoardController.java
@@ -11,7 +11,9 @@ import com.pyo.board_back.dto.response.board.GetBoardResponseDto;
 import com.pyo.board_back.dto.response.board.GetCommentListResponseDto;
 import com.pyo.board_back.dto.response.board.GetFavoriteListResponseDto;
 import com.pyo.board_back.dto.response.board.GetLatestBoardListResponseDto;
+import com.pyo.board_back.dto.response.board.GetSearchBoardListResponseDto;
 import com.pyo.board_back.dto.response.board.GetTop3BoardListResponseDto;
+import com.pyo.board_back.dto.response.board.GetUserBoardListResponseDto;
 import com.pyo.board_back.dto.response.board.IncreaseViewCountResponseDto;
 import com.pyo.board_back.dto.response.board.PatchBoardResponseDto;
 import com.pyo.board_back.dto.response.board.PostBoardResponseDto;
@@ -76,6 +78,25 @@ public class BoardController {
     @GetMapping("/top-3")
     public ResponseEntity<? super GetTop3BoardListResponseDto> getTop3BoardList() {
         ResponseEntity<? super GetTop3BoardListResponseDto> response = boardService.getTop3BoardList();
+        return response;
+    }
+
+    @GetMapping(value = {
+            "/search-list/{searchWord}",
+            "/search-list/{searchWord}/{preSearchWord}"
+    })
+    public ResponseEntity<? super GetSearchBoardListResponseDto> getSearchBoardList(
+            @PathVariable("searchWord") String searchWord,
+            @PathVariable(value = "preSearchWord", required = false) String preSearchWord) {
+        ResponseEntity<? super GetSearchBoardListResponseDto> response = boardService.getSearchBoardList(searchWord,
+                preSearchWord);
+        return response;
+    }
+
+    @GetMapping("/user-board-list/{email}")
+    public ResponseEntity<? super GetUserBoardListResponseDto> getUserBoardList(
+            @PathVariable("email") String email) {
+        ResponseEntity<? super GetUserBoardListResponseDto> response = boardService.getUserBoardList(email);
         return response;
     }
 
