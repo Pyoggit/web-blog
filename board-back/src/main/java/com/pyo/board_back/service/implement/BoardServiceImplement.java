5e986ef653d8239b95abce8be263ff3ff33912ce
Jang Pyo <wkdvy122851@gmail.com>
2025-04-03 19:41:54 +0900

    [UPDATE] 게시글 검색 및 유저별 게시글 조회 서비스 로직 추가

--- board-back/src/main/java/com/pyo/board_back/service/implement/BoardServiceImplement.java
+++ board-back/src/main/java/com/pyo/board_back/service/implement/BoardServiceImplement.java
@@ -19,7 +19,9 @@ import com.pyo.board_back.dto.response.board.GetBoardResponseDto;
 import com.pyo.board_back.dto.response.board.GetCommentListResponseDto;
 import com.pyo.board_back.dto.response.board.GetFavoriteListResponseDto;
 import com.pyo.board_back.dto.response.board.GetLatestBoardListResponseDto;
+import com.pyo.board_back.dto.response.board.GetSearchBoardListResponseDto;
 import com.pyo.board_back.dto.response.board.GetTop3BoardListResponseDto;
+import com.pyo.board_back.dto.response.board.GetUserBoardListResponseDto;
 import com.pyo.board_back.dto.response.board.IncreaseViewCountResponseDto;
 import com.pyo.board_back.dto.response.board.PatchBoardResponseDto;
 import com.pyo.board_back.dto.response.board.PostBoardResponseDto;
@@ -30,11 +32,13 @@ import com.pyo.board_back.entity.BoardListViewEntity;
 import com.pyo.board_back.entity.CommentEntity;
 import com.pyo.board_back.entity.FavoriteEntity;
 import com.pyo.board_back.entity.ImageEntity;
+import com.pyo.board_back.entity.SearchLogEntity;
 import com.pyo.board_back.repository.BoardListViewRepository;
 import com.pyo.board_back.repository.BoardRepository;
 import com.pyo.board_back.repository.CommentRepository;
 import com.pyo.board_back.repository.FavoriteRepository;
 import com.pyo.board_back.repository.ImageRepository;
+import com.pyo.board_back.repository.SearchLogRepository;
 import com.pyo.board_back.repository.UserRepository;
 import com.pyo.board_back.repository.resultSet.GetBoardResultSet;
 import com.pyo.board_back.repository.resultSet.GetCommentListResultSet;
@@ -52,6 +56,7 @@ public class BoardServiceImplement implements BoardService {
     private final ImageRepository imageRepository;
     private final CommentRepository commentRepository;
     private final FavoriteRepository favoriteRepository;
+    private final SearchLogRepository searchLogRepository;
     private final BoardListViewRepository boardListViewRepository;
 
     @Override
@@ -148,6 +153,53 @@ public class BoardServiceImplement implements BoardService {
 
         return GetTop3BoardListResponseDto.success(boardListViewEntities);
 
+    }
+
+    @Override
+    public ResponseEntity<? super GetSearchBoardListResponseDto> getSearchBoardList(String searchWord,
+            String preSearchWord) {
+
+        List<BoardListViewEntity> boardListViewEntities = new ArrayList<>();
+
+        try {
+            boardListViewEntities = boardListViewRepository
+                    .findByTitleContainsOrContentContainsOrderByWriteDatetimeDesc(searchWord, searchWord);
+
+            SearchLogEntity searchLogEntity = new SearchLogEntity(searchWord, preSearchWord, false);
+            searchLogRepository.save(searchLogEntity);
+
+            boolean relation = preSearchWord != null;
+            if (relation) {
+                searchLogEntity = new SearchLogEntity(preSearchWord, searchWord, relation);
+                searchLogRepository.save(searchLogEntity);
+            }
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return GetSearchBoardListResponseDto.success(boardListViewEntities);
+    }
+
+    @Override
+    public ResponseEntity<? super GetUserBoardListResponseDto> getUserBoardList(String email) {
+
+        List<BoardListViewEntity> boardListViewEntities = new ArrayList<>();
+
+        try {
+            boolean existedUser = userRepository.existsByEmail(email);
+            if (!existedUser)
+                return GetUserBoardListResponseDto.noExistUser();
+
+            boardListViewEntities = boardListViewRepository.findByWriterEmailOrderByWriteDatetimeDesc(email);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return GetUserBoardListResponseDto.success(boardListViewEntities);
     }
 
     @Override
